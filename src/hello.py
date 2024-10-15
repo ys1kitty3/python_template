48cb3bbf0b4d1256e952ccaabde0700c8c4cfe3c
[Sonnah Barry] <[mv2218kw@go.minnstate.edu]>
2024-10-15 11:37:35 -0500

    init commit

---
+++ src/hello.py
@@ -0,0 +1,7 @@
+def double(x):
+    return x * 2
+
+def main():
+    print('hello,world')
+    
+main()
---
+++ test/hello_test.py
@@ -0,0 +1,4 @@
+from src.hello import double 
+
+def test_double_zero(): 
+    assert(double(0) == 0)
